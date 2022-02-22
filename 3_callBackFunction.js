dc153b78ac2c0814949e08934199a48608b0d580
Bennison Devadoss <bennisondevadoss@gmail.com>
2022-02-22 10:54:12 +0530

    Third Evaluation

---
+++ 3_callBackFunction.js
@@ -0,0 +1,13 @@
+let callback = () => {
+    console.log(this);   // it gives the window object. 
+}
+
+document.addEventListener("click", callback);
+
+
+
+function callBack() {
+    console.log("Hi I am in Normal function");
+}
+
+document.addEventListener("onresize", callBack)
--- 4_setTimeOut.js
+++ 4_setTimeOut.js
@@ -1,5 +1,7 @@
-for (var i = 1; i < 10; i++) {
+for (let i = 1; i < 10; i++) {
+    let k = i;
     setTimeout(function () {
-        console.log(i);
-    }, 1000 * i);
+        console.log(k);
+    }, 1000 * (k + 1));
 }
+
